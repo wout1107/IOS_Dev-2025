dd898416d50b1ea7ed537b96320c4bddae6175a4
Wout Devriese <woutdevriese@iMacR.local>
2025-09-24 10:46:27 +0200

    tweede les myPlayground nog niet volledig af

--- LES1_playground/MyPlayground.playground/Contents.swift
+++ LES1_playground/MyPlayground.playground/Contents.swift
@@ -31,7 +31,45 @@ func reversedString(text: String) -> String {
     }
 }
 
-print(reversedString(text: firstLastName.voornaam))
+print(reversedString(text: "VIVES"))
 
+func getUpperLowerCount(text: String) ->(uppercase:String, lowercase: String, charcount: Int) {
+    let upper = text.uppercased()
+    let lower = text.lowercased()
+    let char = text.count
+    var result = (upper, lower, char)
+    return result
+}
 
+print(getUpperLowerCount(text: "IOS 26").uppercase)
+print(getUpperLowerCount(text: "IOS 26").lowercase)
+print(getUpperLowerCount(text: "IOS 26").charcount)
+print(getUpperLowerCount(text: "IOS 26"))
 
+func divide(getal1: Int, getal2: Int) -> (Double) {
+    return Double(getal1)/Double(getal2)
+}
+
+print(divide(getal1: 4, getal2: 3))
+
+func calculate(numbers: Int...) -> (avg: Double, min: Int, max: Int, aantal: Int)
+{
+    let min = numbers.min()!
+    let max = numbers.max()!
+    let aantal = numbers.count
+    let sum: Int = numbers.reduce(0, +)
+    let avg = Double(sum)/Double(aantal)
+    return(avg, min, max, aantal)
+}
+
+calculate(numbers: 10,0,5)
+calculate(numbers: 4,5,6,-3)
+calculate(numbers: -3)
+
+var x: Double = 10.0
+var y: Int = 3
+
+func increment(x: inout Double, y: inout Int ) {
+    x = x + 1
+    y = y + 1
+}
